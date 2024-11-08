d1b01b8dda48e5d96da521114da59f5c7334c92c
Maicol <343maicol11@gmail.com>
2024-11-08 12:16:26 -0500

    capturando datos generales de resultados

--- src/main/java/com/catalogo/literalura/LiteraluraApplication.java
+++ src/main/java/com/catalogo/literalura/LiteraluraApplication.java
@@ -1,13 +1,26 @@
 package com.catalogo.literalura;
 
+import com.catalogo.literalura.model.DatosConsumirApi;
+import com.catalogo.literalura.service.ConvertirDatos;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.catalogo.literalura.service.ConsumirApi;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var url = "https://gutendex.com/books/";
+		var consumirApi = new ConsumirApi();
+		var json = consumirApi.datosApi(url);
+		ConvertirDatos conversor = new ConvertirDatos();
+		var datos = conversor.obtenerDatos(json, DatosConsumirApi.class);
+		System.out.println(datos);
+	}
 }
